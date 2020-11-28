1a604ff9952ed071ffb2c3e0198a14ba1e2871cb
Daniel Abdulahad <Daniel00.abdulahad@gmail.com>
2020-11-28 19:21:10 +0100

    Create a table.

--- src/TableView.Test/Program.cs
+++ src/TableView.Test/Program.cs
@@ -3,10 +3,26 @@
 namespace TableView.Test
 {
     class Program
+    internal class Program
     {
         static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Table table = new Table
+            {
+                Margin = 1
+            };
+            table.SetHeader(new TableRow(new string[] { "Row", "Header 2", "Nothing." }));
+
+            table.AddRow(new TableRow(new string[] { "1", "Hello" }));
+            table.AddRow(new TableRow(new string[] { "2", "Hey", "Column 3" }));
+            table.AddRow(new TableRow(new string[] { "3", "Hi", "Extra text", "New Column?" }));
+
+            table.Draw();
+
+            Console.ReadLine();
         }
     }
+}
 }
